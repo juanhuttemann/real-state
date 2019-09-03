2639ec93743e045ae641c430dd3d4fb2f42ce1a3
Juan Huttemann <juanfhuttemann@gmail.com>
2019-09-03 14:56:56 -0400

    add pagination section

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -6,9 +6,21 @@ class Listings extends Component {
         return ( 
             <section id="listings">
                 <section className="search-area">
-
+                    <input type="text" name="search" />
+                </section>
+                <section className="sortby-area">
+                    <div>390 results found</div>
+                    <div className="sort-options">
+                        <select name="sortby" className="sortby">
+                            <option value="price-asc">Highest Price</option>
+                            <option value="price-desc">Lowest Price</option>
+                        </select>
+                    </div>
                 </section>
                 <section className="listings-results">
+
+                </section>
+                <section className="pagination">
 
                 </section>
             </section>
