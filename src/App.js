fe0d3589245d44e54af814ef4095b1d5562ccf10
Juan Huttemann <juanfhuttemann@gmail.com>
2019-09-09 00:55:17 -0400

    starting filter functionality

--- src/App.js
+++ src/App.js
@@ -6,12 +6,30 @@ import Listings from './components/Listings';
 
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      min_price: 0,
+      max_price: 10000000,
+    }
+    this.change = this.change.bind(this)
+  }
+
+  change(event){
+    var name = event.target.name
+    var value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value
+    this.setState({
+      [name]: value
+    }, () => {
+      console.log(this.state)
+    })
+  }
   render () {
     return (
       <div className="App">
         <Header /> 
         <section id="content-area">
-          <Filter />
+          <Filter change={this.change} globalState={this.state} />
           <Listings />
         </section>  
       </div>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,44 +8,53 @@ class Filter extends Component {
             <section id="filter">
                 <div className="inside">
                     <h4>Filter</h4>
+                    <form onChange={this.props.change}> 
                     <select name="neighborhood" className="filters neighborhood">
-                        <option value="">Ridgewood</option>
+                        <option value="Ridgewood">Ridgewood</option>
+                        <option value="Miami">Miami</option>
                     </select>
                     <select name="housetype" className="filters housetype">
-                        <option value="">Ranch</option>
+                        <option value="Ranch">Ranch</option>
+                        <option value="Apartment">Apartment</option>
+                        <option value="Room">Room</option>
+                        <option value="House">House</option>
                     </select>
                     <select name="bedrooms" className="filters bedrooms">
-                        <option value="">2 BR</option>
+                        <option value="1">1 BR</option>
+                        <option value="2">2 BR</option>
+                        <option value="3">3 BR</option>
+                        <option value="4">4 BR</option>
                     </select>
                     <div className="filters price">
                         <span className="title">Price</span>
-                        <input type="text" name="min-price" className="min-price"/>
-                        <input type="text" name="min-price" className="max-price"/>
+                        <input type="text" name="min_price" className="min-price"/>
+                        <input type="text" name="min_price" className="max-price"/>
                     </div>
                     <div className="filters floor-space">
                         <span className="title">Floor Space</span>
-                        <input type="text" name="min-floor-space" className="min-floor-space" />
-                        <input type="text" name="min-floor-space" className="max-floor-space" />
+                        <input type="text" name="min_floor_space" className="min-floor-space" />
+                        <input type="text" name="min_floor_space" className="max-floor-space" />
                     </div>
                     <div className="filters extras">
                         <span className="title">Extras</span>
-                        <label for="extras">
+                        <label htmlFor="extras">
                             <span>Elevators</span>
-                            <input value="elevator" type="checkbox" name="extras"/>
+                            <input value="elevator" type="checkbox" name="elevator"/>
                         </label>
-                        <label for="extras">
+                        <label htmlFor="extras">
                             <span>Swimming Pool</span>
-                            <input value="swimming-pool" type="checkbox" name="extras" />
+                            <input value="swimming_pool" type="checkbox" name="swimming_pool" />
                         </label>
-                        <label for="extras">
+                        <label htmlFor="extras">
                             <span>Finished Basement</span>
-                            <input value="finished-basement" type="checkbox" name="extras" />
+                                <input value="finished_basement" type="checkbox" name="finished_basement" />
                         </label>
-                        <label for="extras">
+                        <label htmlFor="extras">
                             <span>Gym</span>
-                            <input value="gym" type="checkbox" name="extras" />
+                            <input value="gym" type="checkbox" name="gym" />
                         </label>
                     </div>
+                    </form>
                 </div>
             </section>
         );
